101da9dcb90211d827c8c86cce8d79ad65a35e55
tflanigan <tflanig1@gmail.com>
2022-09-26 19:59:27 -0500

    added start of files

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -3,11 +3,11 @@ import pytest
 from resume_automator.data_manager import load_resumes, ResumeData
 
 class TestIntegrationBroad:
-    """Test without any test doubles---full integration.
+    """No test doubles and include filesystem---full integration.
     """
     @pytest.fixture()
     def sample_files(self):
         
 
     def test_json_files(self):
-
+        
