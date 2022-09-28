7fcb3064bc46925352bcc65b18c552ca65296d1c
tflanigan <tflanig1@gmail.com>
2022-09-27 23:34:18 -0500

    Down to failing assertion

--- resume_automator/data_manager.py
+++ resume_automator/data_manager.py
@@ -7,48 +7,49 @@ resume_cache = {}
 
 class ResumeData:
     def __init__(self, config_path, use_cache=True):
-        config_file = Path(config_path).absolute()
-        name = str(config_file)
-        cached = resume_cache.get(name, None)
-        if use_cache and cached:
-            return cached
-
-        self.config_file = config_file
-        self.name = name
+        self.config_file = self._get_config_file(config_path)
         self.use_cache = use_cache
         self.data = {}
-        self.parent = None
+        self.parent_obj = None
         self.patches = []
-        self.reserved_keyword_map = [
-            ['parent-data', self._inherit_from_parent],
-            ['patches', self._apply_patches]
-        ]
         self._load_config(self.config_file)
+
+    def _get_config_file(self, config_path):
+        config_file = Path(config_path).resolve()
+        if not config_file.exists():
+            raise FileNotFoundError
+        return config_file
 
     def _load_config(self, config_path):
         with config_path.open() as f:
-            self.raw_data = json.load(f)
-        for field, funct in self.reserved_keyword_map:
-            value = self.raw_data.get(field)
-            del self.raw_data[field]
-            funct(value)
-        if not self.parent and not self.patches:
-            self.data = self.raw_data.deepcopy()
+            raw_data = json.load(f)
+        parent_ref = raw_data.get('parent-data')
+        patches = raw_data.get('patches', [])
+        if parent_ref:
+            self._inherit_from_parent(parent_ref, patches)
+        else:
+            self._save_to_cache(raw_data)
 
-    def _inherit_from_parent(self, parent_config):
-        self.parent_obj = ResumeData(parent_config)
-        self.data = self.parent_obj.data.deepcopy()
-
-    def _apply_patches(self, patch):
-        self.patch = jsonpatch.JsonPatch(patch)
-        self.data = self.patch.apply(self.data)
-
-    def _save_to_cache(self):
+    def _inherit_from_parent(self, parent_ref, patches):
+        global resume_cache
+        parent_config = self.config_file.parent / parent_ref
         if self.use_cache:
-            resume_cache[self.name] = self
+            cached = resume_cache.get(str(self.config_file))
+            if cached:
+                self.parent_obj = cached
+            else:
+                self.parent_obj = ResumeData(parent_config)
+        bulk_patch = jsonpatch.JsonPatch(patches)
+        self._save_to_cache(bulk_patch.apply(self.parent_obj.data))
+
+    def _save_to_cache(self, data):
+        global resume_cache
+        self.data = deepcopy(data)
+        if self.use_cache and not resume_cache.get(str(self.config_file)):
+            resume_cache[str(self.config_file)] = self
 
 def load_resumes(resume_files):
     resume_datas = {}
     for fp in resume_files:
-        resume_datas[fp] = ResumeData()
+        resume_datas[fp] = ResumeData(fp)
     return resume_datas
--- tests/test_integration.py
+++ tests/test_integration.py
@@ -5,27 +5,28 @@ from pathlib import Path
 from resume_automator.data_manager import load_resumes, ResumeData
 
 # Fixtures that should probably be shared later...
-@pytest.fixture()
-def data_from_json():
-    resume_files = (
-        '../samples/input/json/george_resume.json',
-        '../samples/input/json/george_resume_baseball.json',
-        # ../samples/input/json/george_resume_tv.json'
-    )
-    return load_resumes(resume_files)
-
-@pytest.fixture(params=('../samples/results/json/george_resume.json',
-                        '../samples/results/json/george_resume_baseball.json',
-                        # ../samples/input/json/george_resume_tv.json'
+@pytest.fixture(params=('samples/results/json/george_resume.json',
+                        'samples/results/json/george_resume_baseball.json',
+                        # samples/input/json/george_resume_tv.json'
                        ))
 def result_path(request):
-    return request.params
+    return Path(__file__).parent.parent / request.param
 
 class TestIntegrationBroad:
     """No test doubles and include filesystem---full integration.
     """
-    @pytest.mark.parametrize()
+    @pytest.fixture(scope='class')
+    def data_from_json(self):
+        resume_files = (
+            'samples/input/json/george_resume.json',
+            'samples/input/json/george_resume_baseball.json',
+            # samples/input/json/george_resume_tv.json'
+        )
+        resume_files = [Path(__file__).parent.parent / f for f in resume_files]
+        return load_resumes(resume_files)
+
     def test_json_files(self, data_from_json, result_path):
-        result_obj = json.loads(result_path)
-        test_key = result_path.replace('results', input)
-        assert result_obj == data_from_json[test_key]
+        with result_path.open() as f:
+            result_obj = json.load(f)
+        test_key = Path(str(result_path).replace('results', 'input'))
+        assert result_obj == data_from_json[test_key].data
