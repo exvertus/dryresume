d556299794a0cae1d07c6ee1d00de23a22160f88
tflanigan <tflanig1@gmail.com>
2022-10-02 18:28:05 -0500

    cleanup

--- resume_automator/data_manager.py
+++ resume_automator/data_manager.py
@@ -7,21 +7,21 @@ resume_cache = {}
 
 class ResumeData:
     def __init__(self, config_path, reader, use_cache=True):
-        self.config_file = self._get_config_file(config_path)
+        self.config_file = self.get_config_file(config_path)
         self.use_cache = use_cache
         self.data = {}
         self.parent_obj = None
         self.patches = []
         self.reader = reader
-        self._load_config(self.config_file)
+        self.load_config(self.config_file)
 
-    def _get_config_file(self, config_path):
+    def get_config_file(self, config_path):
         config_file = Path(config_path).resolve()
         if not config_file.exists():
             raise FileNotFoundError
         return config_file
 
-    def _load_config(self, config_path):
+    def load_config(self, config_path):
         with config_path.open() as f:
             raw_data = self.reader(f)
         parent_ref = raw_data.get('parent-data')
@@ -29,7 +29,7 @@ class ResumeData:
         if parent_ref:
             self._inherit_from_parent(parent_ref, patches)
         else:
-            self._save_to_cache(raw_data)
+            self._save(raw_data)
 
     def _inherit_from_parent(self, parent_ref, patches):
         global resume_cache
@@ -41,9 +41,9 @@ class ResumeData:
             else:
                 self.parent_obj = ResumeData(parent_config, self.reader)
         bulk_patch = jsonpatch.JsonPatch(patches)
-        self._save_to_cache(bulk_patch.apply(self.parent_obj.data))
+        self._save(bulk_patch.apply(self.parent_obj.data))
 
-    def _save_to_cache(self, data):
+    def _save(self, data):
         global resume_cache
         self.data = deepcopy(data)
         if self.use_cache and not resume_cache.get(str(self.config_file)):
