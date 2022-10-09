616dcef96553af3b0c427e62ed0438949753d55e
tflanigan <tflanig1@gmail.com>
2022-10-09 00:29:19 -0500

    integration test refactor complete

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,3 +1,4 @@
+from copy import deepcopy
 import json
 import pytest
 import shutil
@@ -5,20 +6,6 @@ import yaml
 from pathlib import Path
 
 from dryresume.resume import create_resumes
-
-def folder_replace(path, search_for, replace_with, limit=1):
-    """Returns path with folder name replaced, starting from deepest.
-    Use limit arg to only replace that many directories.
-    """
-    parts_list = list(path.parts)
-    parts_list.reverse()
-    start_at = 0
-    for i in range(limit):
-        inx = parts_list.index(search_for, start_at)
-        parts_list[inx] = replace_with
-        start_at = inx + 1
-    parts_list.reverse()
-    return Path(*parts_list)
 
 REPO_ROOT = Path(__file__).parent.parent
 
@@ -56,29 +43,44 @@ test_dict = {
         }
     }
 }
+yaml_dict = {
+    'george_resume.yaml': deepcopy(test_dict['base']),
+    'george_resume_baseball.yaml': deepcopy(test_dict['child']),
+    'george_resume_tv.yaml': deepcopy(test_dict['grandchild'])
+}
 
 @pytest.fixture()
 def json_files():
     return [REPO_ROOT / f['readfile'] for f in test_dict.values()]
 
-# @pytest.fixture()
-# def yaml_files(json_files, tmp_path):
-#     for jf in json_files:
-
-#     yamls = [tmp_path / f"{jf.stem}.yaml" for jf in json_files]
-#     for y in yamls:
-#         if not y.exists():
+@pytest.fixture()
+def yaml_files(json_files, tmp_path):
+    result = []
+    for json_path in json_files:
+        file_ext_only = f"{json_path.stem}.yaml"
+        yaml_dest_path = REPO_ROOT / "build" / file_ext_only
+        yaml_src_path = tmp_path / file_ext_only
+        if not yaml_src_path.exists():
+            with json_path.open() as json_file:
+                data = json.load(json_file)
+            data['output-html'] = str(yaml_dest_path)
+            if 'parent-data' in data:
+                data['parent-data'] = \
+                    f"{str(Path(data['parent-data']).stem)}.yaml"
+            with yaml_src_path.open('w+') as yaml_file:
+                yaml.dump(data, yaml_file)
+            yaml_dict[file_ext_only]['readfile'] = yaml_src_path
+        result.append(yaml_src_path)
+    return result
 
 @pytest.fixture()
 def load_jsons(json_files):
     return create_resumes(json_files)
 
-# @pytest.fixture()
-# def load_yamls(yaml_files):
-#     result = []
-#     for yf in yaml_files:
-#         with jf.open('r') as j:
-#             data = json.load(j)
+@pytest.fixture()
+def load_yamls(yaml_files):
+    return create_resumes(
+        yaml_files, reader=lambda x : yaml.load(x, Loader = yaml.Loader))
 
 class TestIntegrationBroad:
     """No test doubles---full integration.
@@ -97,41 +99,11 @@ class TestIntegrationBroad:
                 html_string = f.read()
             assert exp_func(html_string, value)
 
-    # @pytest.fixture()
-    # def json_result(self, json_result_path):
-    #     with json_result_path.open() as f:
-    #         result_obj = json.load(f)
-    #     return result_obj
+    @pytest.mark.parametrize("name", yaml_dict)
+    def test_html_from_yaml(self, load_yamls, name):
+        for value, exp_func in yaml_dict[name]['expected'].items():
+            test_key = yaml_dict[name]['readfile']
+            with load_yamls[test_key].html_target.open() as f:
+                html_string = f.read()
+            assert exp_func(html_string, value)
 
-    # def test_from_json(self, data_from_json, json_result_path, json_result):
-    #     test_key = folder_replace(json_result_path, 'results', 'input')
-    #     assert json_result == data_from_json[test_key].data
-
-    # @pytest.fixture(scope='class')
-    # def data_from_yaml(self):
-    #     resume_files = (
-    #         'samples/input/yaml/george_resume.yaml',
-    #         'samples/input/yaml/george_resume_baseball.yaml',
-    #         'samples/input/yaml/george_resume_tv.yaml'
-    #     )
-    #     resume_files = [Path(__file__).parent.parent / f for f in resume_files]
-    #     return create_resumes(
-    #         resume_files, reader=lambda x : yaml.load(x, Loader = yaml.Loader))
-
-    # @pytest.fixture()
-    # def yaml_result(self, yaml_result_path):
-    #     with yaml_result_path.open() as f:
-    #         result_obj = yaml.load(f, Loader = yaml.Loader)
-    #     return result_obj
-
-    # def test_from_yaml(self, data_from_yaml, yaml_result_path, yaml_result):
-    #     test_key = folder_replace(yaml_result_path, 'results', 'input')
-    #     assert yaml_result == data_from_yaml[test_key].data
-
-    # def test_to_html(self, data_from_yaml, yaml_result_path):
-    #     test_key = folder_replace(yaml_result_path, 'results', 'input')
-    #     with data_from_yaml[test_key].html_target.open() as f:
-    #         result = f.read()
-    #     assert result.startswith('<!DOCTYPE html') and \
-    #         data_from_yaml[test_key].data['contact']['name'] in result and \
-    #         data_from_yaml[test_key].data['contact']['email'] in result
