2d31d9338f7b8d8856532c85cd09a2a5d877bd93
tflanigan <tflanig1@gmail.com>
2023-02-14 16:12:10 -0600

    use docker

--- submit.sh
+++ submit.sh
@@ -1,1 +1,1 @@
-gcloud builds submit --region=us-central1 --project=$GOOGLE_CLOUD_PROJECT --config=cloudbuild.yaml --substitutions=_LOCATION="us-central1"
+gcloud builds submit --region=us-central1 --project=$GOOGLE_CLOUD_PROJECT --config=cloudbuild.yaml --substitutions=_LOCATION="us-central1",_AR_PATH="us-central1-docker.pkg.dev"
