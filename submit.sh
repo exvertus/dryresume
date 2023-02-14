da721f4cf1fca9725fe548bed31da6f2084f6741
tflanigan <tflanig1@gmail.com>
2023-02-14 16:29:48 -0600

    Removed location

--- submit.sh
+++ submit.sh
@@ -1,1 +1,1 @@
-gcloud builds submit --region=us-central1 --project=$GOOGLE_CLOUD_PROJECT --config=cloudbuild.yaml --substitutions=_LOCATION="us-central1",_AR_PATH="us-central1-docker.pkg.dev"
+gcloud builds submit --region=us-central1 --project=$GOOGLE_CLOUD_PROJECT --config=cloudbuild.yaml --substitutions=_AR_PATH="us-central1-docker.pkg.dev"
