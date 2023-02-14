0d6e37ddce4873926d653805241565e86946b9af
tflanigan <tflanig1@gmail.com>
2023-02-14 13:38:01 -0600

    env vars and added keyrings lib

--- submit.sh
+++ submit.sh
@@ -1,1 +1,1 @@
-gcloud builds submit --region=us-central1 --project=$GOOGLE_CLOUD_PROJECT --config=cloudbuild.yaml
+gcloud builds submit --region=us-central1 --project=$GOOGLE_CLOUD_PROJECT --config=cloudbuild.yaml --substitutions=_LOCATION="us-central1"
